7c4e5b703d34068a239d0f8ec282532a095b5e8b
marinastancu <448358@student.fontys.nl>
2023-11-20 14:06:58 +0100

    Initial version of CI/CD pipeline; working on adding AWS S3 support
    Fixing permission error for ./gradlew
    
    Fixing multiple issues
    
    Pushing gradlew executable and removed pipeline step for this
    
    Attempting to fix cache error by chaining jobs in order
    
    Removed unnecessary cache steps from SonarCloud job
    
    Added build step to publish job
    
    Update pipeline.yml
    
    Removing cache

--- src/main/java/com/frankensound/services/AWSService.java
+++ src/main/java/com/frankensound/services/AWSService.java
@@ -1,7 +1,9 @@
 package com.frankensound.services;
 
 import com.frankensound.entities.Song;
+import jakarta.enterprise.context.ApplicationScoped;
 
+@ApplicationScoped
 public class AWSService {
     public Song getByKey(String key) {
         // TODO: Implement returning a link or the file from AWS
